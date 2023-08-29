d071f2fe1a62077279fa56e4ed93764fb6111cf6
vijendra2244 <vs230267@gmail.com>
2023-08-29 21:34:37 +0530

    correct your code

--- prim2.js
+++ prim2.js
@@ -1,12 +1,12 @@
-
-
 let num = 15;
-
-for(let i=0;i<num;i--){
-    if(num % i == 0){
-        console.log("prime");
-    }
-    else{
-        console.log("Not prime")
-    }
+let count = 0;
+for (let i = 0; i < num; i--) {
+  if (num % i == 0) {
+    count++;
+  }
+}
+if (count == 2) {
+  console.log(num, "Is prime");
+} else {
+  console.log(num, "Is not  prime");
 }
