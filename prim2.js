6a0499f12ace06ede551e028436a642d8e0abe8b
vijendra2244 <vs230267@gmail.com>
2023-08-29 21:31:57 +0530

    no

--- prim2.js
+++ prim2.js
@@ -1,12 +1,9 @@
-
-
 let num = 15;
 
-for(let i=0;i<num;i--){
-    if(num % i == 0){
-        console.log("prime");
-    }
-    else{
-        console.log("Not prime")
-    }
+for (let i = 0; i < num; i--) {
+  if (num % i == 0) {
+    console.log("prime");
+  } else {
+    console.log("Not prime");
+  }
 }
